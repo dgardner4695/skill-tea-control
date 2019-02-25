4ba7bb6e754cf3a7ac2e77c865512b0be9eeee2a
Daniel Gardner <gardner.dan@husky.neu.edu>
2019-02-25 16:34:45 -0500

    Added initial version of remaining demo intents

--- __init__.py
+++ __init__.py
@@ -25,11 +25,13 @@ class TeaControlSkill(MycroftSkill):
         super(TeaControlSkill, self).__init__(name='TeaControlSkill')
         
         # Initialize working variables used within the skill.
-        self.CE_status = -1
+        self.CE_status = 'off'
         self.ser = serial.Serial('/dev/serial0', baudrate=115200, dsrdtr=False, timeout=0.25)
         self.gas_level = 0
         self.rpm = 0
         self.pressure = 0
+        self.locked = 'locked'
+        self.engine_temp = 50
         self.inf = inflect.engine()
 
     @intent_handler(IntentBuilder('').require('CheckEngine').optionally('OnOff'))
@@ -43,9 +45,9 @@ class TeaControlSkill(MycroftSkill):
 
             stat = self.ser.read(1)
 
-            if stat.decode('utf-8') is '1':
+            if stat.decode('utf-8') == '1':
                 self.CE_status = 'on'
-            elif stat.decode('utf-8') is '0':
+            elif stat.decode('utf-8') == '0':
                 self.CE_status = 'off'
         else:
             self.ser.write('checkengine_light {}\n'.format(on_off).encode())
@@ -80,6 +82,19 @@ class TeaControlSkill(MycroftSkill):
 
         self.speak_dialog('rpm.read', data={'measure': self.rpm})
 
+    @intent_handler(IntentBuilder('').require('TempCheck'))
+    def handle_engine_temp_intent(self, message):
+        
+        self.ser.reset_input_buffer()
+        self.ser.write(b'analogread A4\n')
+
+        stat = self.ser.read(4)
+
+        temp_int = int(stat.decode('utf-8').split()[0])
+        self.engine_temp = self.inf.number_to_words(round(temp_int / 1024) * 100)
+
+        self.speak_dialog('engine.temp', data={'measure': self.engine_temp})
+
     @intent_handler(IntentBuilder('').require('WhichTire').require('TirePressure'))
     def handle_tire_pressure_intent(self, message):
 
@@ -95,6 +110,41 @@ class TeaControlSkill(MycroftSkill):
         self.pressure = self.inf.number_to_words(round((pressure_int / 1024) * 35))
 
         self.speak_dialog('tire.pressure', data={'whichtire': tire_string, 'pressure': self.pressure})
+
+    @intent_handler(IntentBuilder('').require('Headlights').require('OnOff'))
+    def handle_headlight_intent(self, message):
+
+        on_off = message.data['OnOff']
+        
+        self.ser.reset_input_buffer()
+        self.ser.write('headlights {}\n'.format(on_off).encode())
+
+        self.speak_dialog('head.light', data={'state': on_off})
+
+    @intent_handler(IntentBuilder('').require('OpenClose').require('Windows'))
+    def handle_window_adjust_intent(self, message):
+
+        open_close = message.data['OpenClose']
+        
+        self.ser.reset_input_buffer()
+        self.ser.write('window {}\n'.format(open_close).encode())
+
+        self.speak_dialog('window.adjust', data={'state': open_close})
+
+    @intent_handler(IntentBuilder('').require('LockCheck'))
+    def handle_headlight_intent(self, message):
+        
+        self.ser.reset_input_buffer()
+        self.ser.write(b'digitalread 12\n')
+
+        stat = self.ser.read(1)
+
+        if stat == '1':
+            self.locked = 'locked'
+        else:
+            self.locked = 'unlocked'
+
+        self.speak_dialog('locked.unlocked', data={'state': self.locked})
 
     def stop(self):
         if (self.ser.isOpen()):
