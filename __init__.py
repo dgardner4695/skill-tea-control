af876afda2fc6d5da948bfea7838a5502d076230
Daniel Gardner <gardner.dan@husky.neu.edu>
2019-02-26 13:36:56 -0500

    Add check to Engine OnOff presence in CheckEngine intent

--- __init__.py
+++ __init__.py
@@ -1,12 +1,6 @@
 # TODO: Add an appropriate license to your skill before publishing.  See
 # the LICENSE file for more information.
 
-# Below is the list of outside modules you'll be using in your skill.
-# They might be built-in to Python, from mycroft-core or from external
-# libraries.  If you use an external library, be sure to include it
-# in the requirements.txt file so the library is installed properly
-# when the skill gets installed later by a user.
-
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
@@ -14,10 +8,9 @@ import serial
 import inflect
 import time
 
-# Each skill is contained within its own class, which inherits base methods
-# from the MycroftSkill class.  You extend this class as shown below.
+# Each intent controls or accesses a specific system of the car
 
-# TODO: Change "Template" to a unique name for your skill
+# TEA (Third Eye Auto) Control Skill
 class TeaControlSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
@@ -37,10 +30,13 @@ class TeaControlSkill(MycroftSkill):
     @intent_handler(IntentBuilder('').require('CheckEngine').optionally('OnOff'))
     def handle_check_eng_intent(self, message):
 
-        on_off = message.data['OnOff']
+        if 'OnOff' in message.data.keys():
+            on_off = message.data['OnOff']
+        else:
+            on_off = None
 
         self.ser.reset_input_buffer()
-        if not on_off:
+        if on_off is None:
             self.ser.write(b'checkengine_status\n')
 
             stat = self.ser.read(1)
@@ -151,7 +147,5 @@ class TeaControlSkill(MycroftSkill):
             self.ser.close()
         return True
 
-# The "create_skill()" method is used to create an instance of the skill.
-# Note that it's outside the class itself.
 def create_skill():
     return TeaControlSkill()
